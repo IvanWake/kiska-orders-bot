1708e0f9b6de9404ba83ec4adb92813059f86b16
Ivan Timofeev <purplebaby16@vk.com>
2026-01-03 19:07:37 +0300

    Fix icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Виш-лист | Список желаний",
+  title: "Хочу вкусняшек",
   description: "Создавайте и управляйте вашими желаниями",
   generator: "v0.app",
   icons: {
